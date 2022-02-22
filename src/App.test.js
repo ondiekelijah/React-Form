f88f75cec46f1ba73bb635b4118cea18e6111101
Dev-Elie <eochieng9448@gmail.com>
2022-02-22 15:49:20 +0300

    Reworked responsiveness

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('test form rendering', () => {
-    render(<App />);
-    const linkElement = screen.getByText(/Sign Up/i);
-    expect(linkElement).toBeInTheDocument();
-});
